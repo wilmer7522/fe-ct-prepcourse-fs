f9bc9f9d707e451e3d36c031da93d1b1cf229aae
wilmer7522 <wilmer7522@gmail.com>
2023-02-19 11:33:38 -0500

    Update homework.js

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -12,6 +12,7 @@ function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -19,6 +20,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -27,6 +29,10 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   var suma = arrayOfNumbers.reduce(function(acu, elem){
+      return acu + elem;
+   });
+   cb (suma);
 }
 
 function forEach(array, cb) {
@@ -34,6 +40,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach(function(ele){
+      cb(ele);
+   });
 }
 
 function map(array, cb) {
@@ -41,12 +50,19 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   var nuevoArray = array.map(function(ele){
+      return cb(ele);
+   });
+   return nuevoArray;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   return arrayOfStrings.filter(function(str){
+      return (str[0] === 'a');
+   })
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
